b868dc139f537cd8e5f782c45447486686563eca
Michael Herb <mherb@ciena.com>
2018-12-04 11:58:14 -0500

    add a find by contributor name method

--- src/main/java/com/ciena/recipemanager/repository/RecipeRepository.java
+++ src/main/java/com/ciena/recipemanager/repository/RecipeRepository.java
@@ -3,7 +3,9 @@ package com.ciena.recipemanager.repository;
 import com.ciena.recipemanager.domain.Recipe;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface RecipeRepository extends MongoRepository<Recipe, String> {
-    Recipe findByContributorName(String contributorName);
-    Recipe findByTitle(String title);
+    Optional<Recipe> findByContributorName(String contributorName);
+    Optional<Recipe> findByTitle(String title);
 }
