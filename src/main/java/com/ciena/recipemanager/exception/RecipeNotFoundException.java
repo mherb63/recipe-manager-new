7830a840295834e8c86a630e74810999fbfda119
Michael Herb <mherb@ciena.com>
2018-12-04 11:55:13 -0500

    add ability to use this exception for more that just recipe not found by ID

--- src/main/java/com/ciena/recipemanager/exception/RecipeNotFoundException.java
+++ src/main/java/com/ciena/recipemanager/exception/RecipeNotFoundException.java
@@ -4,8 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class RecipeNotFoundException extends RuntimeException {
-    public RecipeNotFoundException(String id) {
-        super("Could not find recipe with id:  " + id);
-        log.error("Could not find recipe with id: {}", id);
+    public RecipeNotFoundException(String msg) {
+        super(msg);
+        log.error(msg);
     }
 }
